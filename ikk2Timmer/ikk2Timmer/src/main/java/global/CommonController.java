2a4e8710675786b235f8ea58a6dc9f6a395460e1
pea06 <101560095+pea06@users.noreply.github.com>
2022-11-02 17:44:19 +0900

    예외처리

---
+++ ikk2Timmer/ikk2Timmer/src/main/java/global/CommonController.java
@@ -0,0 +1,23 @@
+package global;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class CommonController {
+    @GetMapping("error/errorCommon")
+    public void handleCommonError() {
+        log.info("errorCommon");
+    }
+
+    @GetMapping("error/accessErroer")
+    public void accdssDenied(Authentication authentication, Model model) {
+        log.info(String.valueOf(authentication));
+
+        model.addAttribute("msg","AccessDenied");
+    }
+}
---
+++ ikk2Timmer/ikk2Timmer/src/main/java/global/CommonExceptionHandler.java
@@ -0,0 +1,10 @@
+package global;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@Slf4j
+@ControllerAdvice
+public class CommonExceptionHandler {
+
+}
