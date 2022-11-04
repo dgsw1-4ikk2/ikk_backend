763882052aa0dc72fba340bc69258ff54bfc82aa
pea06 <101560095+pea06@users.noreply.github.com>
2022-11-04 16:42:01 +0900

    totalRank 추가

--- ikk2Timmer/ikk2Timmer/src/main/java/com/example/ikk2Timmer/Service/Impl/UserServiceImpl.java
+++ ikk2Timmer/ikk2Timmer/src/main/java/com/example/ikk2Timmer/Service/Impl/UserServiceImpl.java
@@ -1,16 +1,22 @@
 package com.example.ikk2Timmer.Service.Impl;
 
+import com.example.ikk2Timmer.Domain.Record;
 import com.example.ikk2Timmer.Domain.User;
+import com.example.ikk2Timmer.Dto.RecordDto;
 import com.example.ikk2Timmer.Repository.UserRepository;
+import com.example.ikk2Timmer.Security.UserDetailsImpl;
 import com.example.ikk2Timmer.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.security.Principal;
 
 @Service
+@Transactional
 public class UserServiceImpl implements UserService {
 
     @Autowired
@@ -19,6 +25,9 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Override
     public void signUpUser(User user) {
         String encodedPassword =  passwordEncoder.encode(user.getPassword());
@@ -26,4 +35,21 @@ public class UserServiceImpl implements UserService {
         userRepository.save(user);
     }
 
+    @Override
+    public void saveRecord(Principal principal, final RecordDto recordDto) {
+        UserDetails userDetails = findUserDetails(principal.getName());
+        if (!(userDetails instanceof UserDetailsImpl)) {
+            throw new IllegalArgumentException();
+        }
+
+        User user = ((UserDetailsImpl) userDetails).getUser();
+        Record record = recordDto.toEntity();
+        user.addRecord(record);
+        userRepository.save(user);
+    }
+
+    @Transactional(readOnly = true)
+    protected UserDetails findUserDetails(String name) {
+        return userDetailsService.loadUserByUsername(name);
+    }
 }
